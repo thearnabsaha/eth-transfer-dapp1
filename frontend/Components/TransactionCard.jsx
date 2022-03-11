7814f3c22a29b31654050b4686ac050fe467e0c9
thearnabsaha <thearnabsaha201@gmail.com>
2022-03-11 19:50:35 +0530

    s1

--- frontend/Components/TransactionCard.jsx
+++ frontend/Components/TransactionCard.jsx
@@ -2,10 +2,10 @@ const TransactionCard = () => {
     return (
         <>
             <div id="card" className="blue-glassmorphism">
-                <p>From: <a href="#">0x78246387fhf24317891432</a></p>
-                <p>To: <a href="#">0x78246387fhf24317891432</a></p>
+                <p>From: <a href="#">0x78247...34432</a></p>
+                <p>To: <a href="#">0x78247...34432</a></p>
                 <p>Amount: 0.001 ETH</p>
-                <p>2/2/2022,2:22:22 AM</p>
+                <h3>2/2/2022,2:22:22 AM</h3>
             </div>
         </>
     );
